60f0a138360bef27d5dc37531cf27ddd293385c2
Nick Widmann <njwidmann@gmail.com>
2015-08-22 13:53:19 -0400

    Google play integration. Chartboost ads. Other changes

--- andengine/src/main/java/org/andengine/entity/shape/RectangularShape.java
+++ andengine/src/main/java/org/andengine/entity/shape/RectangularShape.java
@@ -8,9 +8,9 @@ import org.andengine.util.algorithm.collision.RectangularShapeCollisionChecker;
 /**
  * (c) 2010 Nicolas Gramlich
  * (c) 2011 Zynga Inc.
- * 
- * @author Nicolas Gramlich
- * @since 11:37:50 - 04.04.2010
+ *
+ * @author Nicolas Gramlich, Nick Widmann
+ * @since 11:37:50 - 04.04.2010, 2015
  */
 public abstract class RectangularShape extends Shape implements IAreaShape {
 	// ===========================================================
@@ -20,6 +20,8 @@ public abstract class RectangularShape extends Shape implements IAreaShape {
 	// ===========================================================
 	// Fields
 	// ===========================================================
+
+	public enum Side {LEFT, RIGHT, TOP, BOTTOM, CENTER}
 
 	protected float mWidth;
 	protected float mHeight;
@@ -43,23 +45,87 @@ public abstract class RectangularShape extends Shape implements IAreaShape {
 	// Getter & Setter
 	// ===========================================================
 
+	/**
+	 * Overloaded method to set center, left, and rightmost x values
+	 * @param side Side.CENTER, Side.RIGHT or Side.LEFT
+	 * @param x the x value
+	 */
+	public void setX(Side side, float x) {
+		if(side == Side.RIGHT) {
+			setX(x - this.getWidth());
+		} else if (side == Side.CENTER) {
+			setX(x - this.getWidth() / 2);
+		} else {
+			setX(x);
+		}
+	}
+
+	/**
+	 * Overloaded method to set the center, top, and bottommost y values
+	 * @param side Side.CENTER, Side.TOP or Side.BOTTOM
+	 * @param y the y value
+	 */
+	public void setY(Side side, float y) {
+		if(side == Side.BOTTOM) {
+			setY(y - this.getHeight());
+		} else if (side == Side.CENTER) {
+			setY(y - this.getHeight() / 2);
+		} else {
+			setY(y);
+		}
+	}
+
+
+
+
+	/**
+	 * Overloaded method to get center, left, and rightmost x values
+	 * @param side Side.CENTER, Side.RIGHT or Side.LEFT
+	 * @return the x value
+	 */
+	public float getX(Side side) {
+		if(side == Side.RIGHT) {
+			return this.getX() + this.getWidth();
+		} else if (side == Side.CENTER) {
+			return this.getX() + this.getWidth() / 2;
+		} else {
+			return this.getX();
+		}
+	}
+
+	/**
+	 * Overloaded method to get the center, top, and bottommost y values
+	 * @param side Side.CENTER, Side.TOP or Side.BOTTOM
+	 * @return the y value
+	 */
+	public float getY(Side side) {
+		if(side == Side.BOTTOM) {
+			return this.getY() + this.getHeight();
+		} else if (side == Side.CENTER) {
+			return this.getY() + this.getHeight() / 2;
+		} else {
+			return this.getY();
+		}
+	}
+
+
 	@Override
 	public float getWidth() {
 		return this.mWidth;
 	}
 
-    @Override
-    public float getBaseWidth() {return this.mWidth;}
+	@Override
+	public float getBaseWidth() {return this.mWidth;}
 
 	@Override
 	public float getHeight() {
 		return this.mHeight;
 	}
 
-    @Override
-    public float getBaseHeight() {
-        return this.mHeight;
-    }
+	@Override
+	public float getBaseHeight() {
+		return this.mHeight;
+	}
 
 	@Override
 	public void setWidth(final float pWidth) {
